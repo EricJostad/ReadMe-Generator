2449d6ec95f222f7b8a051cbeec61a08e3e55540
EJostad <eric.jostad@outlook.com>
2021-01-18 23:30:31 -0800

    Added initial questions to array of objects in index.js which will handle user input on the command line.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,44 @@
-// TODO: Include packages needed for this application
+// Required Node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: 'What is the title of the application?',
+            name: 'title',
+            // Validation check to ensure user does not miss any necessary inputs
+            validate: (value)=> {if (value){ return true} 
+            else {return 'We need a value here to proceed.'}}
+        },
+        {
+            type: 'input',
+            message: 'What is the description of the application?',
+            name: 'description',
+            // Validation check to ensure user does not miss any necessary inputs
+            validate: (value)=> {if (value){ return true} 
+            else {return 'We need a value here to proceed.'}}
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions?',
+            name: 'installation',
+            // Validation check to ensure user does not miss any necessary inputs
+            validate: (value)=> {if (value){ return true} 
+            else {return 'We need a value here to proceed.'}}
+        }
+        ,        {
+            type: 'input',
+            message: 'What are the contribution guidelines?',
+            name: 'contributions',
+            // Validation check to ensure user does not miss any necessary inputs
+            validate: (value)=> {if (value){ return true} 
+            else {return 'We need a value here to proceed.'}}
+        }
+    )
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
