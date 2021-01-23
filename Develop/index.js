93eb1491ef6dba21779c4dd2f9a034c56292c809
EJostad <eric.jostad@outlook.com>
2021-01-22 21:13:58 -0800

    Updated formatting for document for increased readability.

--- Develop/index.js
+++ Develop/index.js
@@ -5,81 +5,99 @@ const fs = require('fs');
 
 // // TODO: Create an array of questions for user input
 const questions = [
-        {
-            type: 'input',
-            message: 'What is the title of the application?',
-            name: 'title',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value){ return true} 
-            else {return 'Please specify a title for your application.'}}
-        },
-        {
-            type: 'input',
-            message: 'What is the description of the application?',
-            name: 'description',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.length > 10){ return true} 
-            else {return 'Please provide a bit more detail.'}}
-        },
-        {
-            type: 'input',
-            message: 'What are the installation instructions?',
-            name: 'installation',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.length > 10){ return true} 
-            else {return 'Please provide a bit more detail.'}}
-        },
-        {
-            type: 'input',
-            message: 'What are the contribution guidelines?',
-            name: 'contributions',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.length > 10){ return true} 
-            else {return 'Please provide a bit more detail.'}}
-        },
-        {
-            type: 'input',
-            message: 'What are the test instructions?',
-            name: 'instructions',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.length > 10){ return true} 
-            else {return 'Please provide a bit more detail.'}}
-        },
-        {
-            type: 'input',
-            message: 'How do you start and use the application?',
-            name: 'usage',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.length > 10){ return true} 
-            else {return 'Please provide a bit more detail.'}}
-        },
-        {
-            type: 'list',
-            message: 'Which license was used?',
-            name: 'license',
-            choices: ['MIT','test','test','test','test'],
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value){ return true} 
-            else {return 'Please select a license.'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your github?',
-            name: 'github',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value){ return true} 
-            else {return 'We share your GitHub information.'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your email?',
-            name: 'email',
-            // Validation check to ensure user does not miss any necessary inputs
-            validate: (value)=> {if (value.includes('@')){ return true} 
-            else {return 'Please enter in a valid email.'}}
+    {
+        type: 'input',
+        message: 'What is the title of the application?',
+        name: 'title',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value) { return true }
+            else { return 'Please specify a title for your application.' }
         }
-    ]
-    
+    },
+    {
+        type: 'input',
+        message: 'What is the description of the application?',
+        name: 'description',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.length > 10) { return true }
+            else { return 'Please provide a bit more detail.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.length > 10) { return true }
+            else { return 'Please provide a bit more detail.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contributions',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.length > 10) { return true }
+            else { return 'Please provide a bit more detail.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'instructions',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.length > 10) { return true }
+            else { return 'Please provide a bit more detail.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'How do you start and use the application?',
+        name: 'usage',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.length > 10) { return true }
+            else { return 'Please provide a bit more detail.' }
+        }
+    },
+    {
+        type: 'list',
+        message: 'Which license was used?',
+        name: 'license',
+        choices: ['MIT', 'test', 'test', 'test', 'test'],
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value) { return true }
+            else { return 'Please select a license.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is your github?',
+        name: 'github',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value) { return true }
+            else { return 'We share your GitHub information.' }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+        // Validation check to ensure user does not miss any necessary inputs
+        validate: (value) => {
+            if (value.includes('@')) { return true }
+            else { return 'Please enter in a valid email.' }
+        }
+    }
+]
+
 // ),then((
 //     title,
 //     description,
@@ -110,22 +128,6 @@ const questions = [
 // # GitHub: ${github}
 // # E-Mail: ${email}`;
 
-// // This function will create our README using fs
-// createNewFile(title, template);
-// }
-// );
-
-// // Creating our createNewFile function
-// function createNewFile(fileName, data){
-//     // fs
-//     fs.writeFile(`./${fileName.toLowerCase().split('').join('')}.md`, data,()=>{
-//         if(err){
-//             console.log(err)
-//         }
-//         console.log('Your README has been generated.');
-//     })
-
-//     }
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, 'utf8', (err) => console.log('Error in write file: ', err));
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let result;
+  switch (license) {
+    case 'MIT':
+      result = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Mozilla':
+      result = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+    default:
+      result = '';
+      break;
+  }
+  return result;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,9 +31,9 @@ function renderLicenseSection(license) {
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(title, repository, license) {
+function generateMarkdown(title, description, license) {
   return `# ${title} /n
-  reporsitory: ${repository} /n
+  description: ${description} /n
   license: ${renderLicenseSection(license)}
 `;
 }
